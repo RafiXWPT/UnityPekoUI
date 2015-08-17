477422f1c3603e76c5976ef58fccf1c75835fd52
RafiXWPT <mail.rafixwpt@gmail.com>
2015-08-18 01:04:12 +0200

    Revert "Movement handle & Change coins look"
    This reverts commit c9c49e635aadfee408fac060e55b2d063c5a0518.

--- Peko UI/Assets/Scripts/Player/PlayerController.cs
+++ Peko UI/Assets/Scripts/Player/PlayerController.cs
@@ -3,41 +3,13 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float PlayerSpeed;
-	public float RotateSpeed;
-
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
 	void Update () {
 	
-	}
-
-	void FixedUpdate()
-	{
-		if(Input.GetKey(KeyCode.W))
-		{
-			Move (Vector3.forward);
-		}
-		else if (Input.GetKey(KeyCode.S))
-		{
-			Move (Vector3.back);
-		}
-		
-		if(Input.GetKey(KeyCode.A))
-		{
-			Rotate(Vector3.down);
-		}
-		else if (Input.GetKey(KeyCode.D))
-		{
-			Rotate(Vector3.up);
-		}
-	}
-
-	void Move(Vector3 direction)
-	{
-		this.transform.Translate(direction * PlayerSpeed * Time.deltaTime);
-	}
-	
-	void Rotate(Vector3 direction)
-	{
-		this.transform.Rotate(direction * RotateSpeed * Time.deltaTime);
 	}
 }
